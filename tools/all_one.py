af5bd29bf1b1e3988b0bc56c71108b6e5536380d
Sreethyan <aravinthyan@outlook.com>
2020-10-19 22:34:01 +0100

    removed tools as it was not used for the project at the end

--- tools/all_one.py
+++
@@ -1,3 +0,0 @@
-with open("out.mem", "w") as my_file:
-    for i in range(32):
-        my_file.write("1111111111111111\n")
--- tools/append_0.py
+++
@@ -1,3 +0,0 @@
-with open("out.mem", "a") as my_file:
-    for i in range(22):
-        my_file.write("0000000000000000\n")
